bef22bf31abdba73d1283d794c7b3b1fc0effefe
Navdeep Singh <Navdeep Singh>
2016-09-04 07:36:20 -0700

    Fix for DB reference to load in packaged and unpackaged state

--- desktop_www/js/desktop_scripts.js
+++ desktop_www/js/desktop_scripts.js
@@ -1,7 +1,9 @@
-var ipc = require("electron").ipcRenderer;
-var fs = require('fs');
-var bfr = fs.readFileSync('gurbani.sqlite');
-    db = new SQL.Database(bfr);
+var ipc   = require("electron").ipcRenderer;
+var fs    = require('fs');
+var path  = require('path');
+var file  = path.join(__dirname, "../gurbani.sqlite");
+var bfr   = fs.readFileSync(file);
+    db    = new SQL.Database(bfr);
 
 function sendText(text) {
   ipc.send("show-line", {line: text})
