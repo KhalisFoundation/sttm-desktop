c9205f3b350364ce02e32f9e9ec451e7d5d53407
Navdeep Singh <info@cyfersystems.com>
2016-11-25 09:19:48 -0800

    Use CSS class for Gurmukhi lines in viewer instead of whole document Show English SSK translation, transliteration, and SGGS Darpan

--- desktop_www/js/viewer.js
+++ desktop_www/js/viewer.js
@@ -1,11 +1,22 @@
+var currentShabad,
+    currentLine;
+
 //IPC
 var ipc = require("electron").ipcRenderer;
 ipc.on("show-line", function(event, data) {
-  makeSlide([$("<h1></h1>").text(data.lineID)]);
+  var content = db.exec("SELECT gurmukhi, english_ssk, transliteration, sggs_darpan FROM shabad WHERE _id = " + data.lineID);
+  var item = content[0].values[0];
+  
+  makeSlide([
+    $("<h1></h1>").addClass("gurmukhi").text(item[0]),
+    $("<h2></h2>").css("color","#fcf").text(item[1]),
+    $("<h2></h2>").css("color","#ffc").text(item[2]),
+    $("<h2></h2>").css("color","#cff").text(item[3]),
+  ]);
 });
 
 ipc.on("show-text", function(event, data) {
-  makeSlide([$("<h1></h1>").text(data.text)]);
+  makeSlide([$("<h1></h1>").addClass("gurmukhi").text(data.text)]);
 })
 
 function makeSlide(appendObj) {
