3c5a10b6bba8331a785bb8ab072c824f9f48c0cf
Navdeep Singh <me@navdeepsingh.ca>
2016-12-21 14:20:04 -0800

    Some ES5 -> ES6 Open viewer when displaying Shabad instead of all the time Show viewer as fullscreen when second monitor available Prevent overscrolling in viewer

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var electron      = require("electron");
-var app           = require("app");
-var BrowserWindow = require("browser-window");
-var ipc           = require("electron").ipcMain;
-var appVersion    = require("./package.json").version;
-var os            = require("os").platform();
-var mainWindow, viewerWindow;
+const electron        = require("electron");
+const app             = require("app");
+const {BrowserWindow} = electron;
+const ipc             = electron.ipcMain;
+const appVersion      = require("./package.json").version;
+const os              = require("os").platform();
+let mainWindow,
+    viewerWindow,
+    viewerWindowOpen  = false,
+    viewerWindowX     = 50,
+    viewerWindowY     = 50,
+    viewerWindowFS    = false;
 
-if(require("electron-squirrel-startup")) return;
-var autoUpdater   = require("auto-updater");
+if (require("electron-squirrel-startup")) return;
+const autoUpdater   = require("auto-updater");
 
 if (process.env.NODE_ENV !== "development") {
   updateFeed = "http://releases.khalis.net/sttme/" + os + "/";
@@ -24,9 +29,9 @@ autoUpdater.on("update-downloaded", function (e, releaseNotes, releaseName, rele
 });
 
 app.on("ready", function () {
-  var electronScreen = electron.screen;
-  var displays = electronScreen.getAllDisplays();
-  var externalDisplay = null;
+  const electronScreen  = electron.screen;
+  const displays        = electronScreen.getAllDisplays();
+  let externalDisplay   = null;
   for (var i in displays) {
     if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
       externalDisplay = displays[i];
@@ -36,21 +41,22 @@ app.on("ready", function () {
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 600,
+    show: false,
     titleBarStyle: "hidden"
+  })
+  mainWindow.webContents.on('did-finish-load', () => {
+    console.log('ready-to-show mainWindow');
+    mainWindow.show();
   })
   mainWindow.loadURL("file://" + __dirname + "/www/index.html");
 
-  viewerWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    x: 50,
-    y: 50,
-    //x: externalDisplay.bounds.x + 50,
-    //y: externalDisplay.bounds.y + 50,
-    //fullscreen: true,
-  });
-  viewerWindow.loadURL("file://" + __dirname + "/desktop_www/viewer.html");
+  if (externalDisplay) {
+    viewerWindowX   = externalDisplay.bounds.x + 50,
+    viewerWindowY   = externalDisplay.bounds.y + 50,
+    viewerWindowFS  = true;
+  }
 });
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -61,12 +67,48 @@ app.on('window-all-closed', () => {
   //}
 });
 
+function createViewer(ipcData) {
+  console.log(typeof ipcData);
+  viewerWindow = new BrowserWindow({
+    width       : 800,
+    height      : 600,
+    x           : viewerWindowX,
+    y           : viewerWindowY,
+    fullscreen  : viewerWindowFS,
+    show        : false,
+  });
+  viewerWindow.loadURL("file://" + __dirname + "/desktop_www/viewer.html");
+  viewerWindow.webContents.on("did-finish-load", () => {
+    viewerWindow.show();
+    viewerWindowOpen = true;
+    if (typeof ipcData !== "undefined") {
+      viewerWindow.webContents.send(ipcData.send, ipcData.data);
+    }
+  });
+  viewerWindow.on("closed", () => {
+    viewerWindowOpen = false;
+    viewerWindow = null;
+  });
+}
+
 ipc.on('show-line', function(event, arg) {
-  console.log('show-line app.js');
-  viewerWindow.webContents.send('show-line', arg);
+  if (viewerWindowOpen) {
+    viewerWindow.webContents.send("show-line", arg);
+  } else {
+    createViewer({
+      send: "show-line",
+      data: arg
+    });
+  }
 });
 
 ipc.on("show-text", function(event, arg) {
-  console.log("show-text app.js");
-  viewerWindow.webContents.send("show-text", arg);
+  if (viewerWindowOpen) {
+    viewerWindow.webContents.send("show-text", arg);
+  } else {
+    createViewer({
+      send: "show-text",
+      data: arg
+    })
+  }
 });
