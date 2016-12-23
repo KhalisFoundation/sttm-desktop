949845be81f0375b015a6200706cc134df8fee2f
Navdeep Singh <me@navdeepsingh.ca>
2016-12-23 15:46:35 -0800

    Increment version Remove sqlite3 Use better-sqlite3 Pull latest HTML Fix font paths for viewer

--- desktop_www/js/viewer.js
+++ desktop_www/js/viewer.js
@@ -4,14 +4,14 @@ var currentShabad,
 //IPC
 var ipc = require("electron").ipcRenderer;
 ipc.on("show-line", function(event, data) {
-  db.get("SELECT gurmukhi, english_ssk, transliteration, sggs_darpan FROM shabad WHERE _id = " + data.lineID, function(err, row) {
-      makeSlide([
-        $("<h1></h1>").addClass("gurmukhi").text(row.gurmukhi),
-        $("<h2></h2>").css("color","#fcf").text(row.english_ssk),
-        $("<h2></h2>").css("color","#ffc").text(row.transliteration),
-        $("<h2></h2>").css("color","#cff").text(row.sggs_darpan),
-      ]);
-  });
+  var stmt = db.prepare("SELECT gurmukhi, english_ssk, transliteration, sggs_darpan FROM shabad WHERE _id = " + data.lineID);
+  var row = stmt.get();
+  makeSlide([
+    $("<h1></h1>").addClass("gurmukhi").text(row.gurmukhi),
+    $("<h2></h2>").css("color","#fcf").text(row.english_ssk),
+    $("<h2></h2>").css("color","#ffc").text(row.transliteration),
+    $("<h2></h2>").css("color","#cff").text(row.sggs_darpan),
+  ]);
 });
 
 ipc.on("show-text", function(event, data) {
