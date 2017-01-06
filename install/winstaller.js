d0e1497377b77cc018bca2153f779b3653209cb4
Navdeep Singh <me@navdeepsingh.ca>
2017-01-06 09:27:03 -0800

    Update winstaller settings GIF for Windows installer

--- install/winstaller.js
+++ install/winstaller.js
@@ -1,10 +1,19 @@
 var electronInstaller = require('electron-winstaller');
+var path = require('path');
+
+var rootPath = path.join('./');
+var buildDir = path.join(rootPath, 'builds');
+var assetsDir = path.join(rootPath, 'assets');
 
 resultPromise = electronInstaller.createWindowsInstaller({
-  appDirectory: './builds/SikhiToTheMax-win32-x64',
-  outputDirectory: './builds/SikhiToTheMax64',
+  appDirectory: path.join(buildDir, 'SikhiToTheMax-win32-x64'),
+  outputDirectory: path.join(buildDir, 'SikhiToTheMax64'),
   authors: 'Khalis, Inc.',
-  exe: 'SikhiToTheMax.exe'
+  exe: 'SikhiToTheMax.exe',
+  noMsi: true,
+  setupExe: 'SikhiToTheMaxSetup.exe',
+  setupIcon: path.join(assetsDir, 'STTME.ico'),
+  loadingGif: path.join(assetsDir, 'sttm-install.gif')
 });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
