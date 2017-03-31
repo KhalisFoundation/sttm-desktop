0fc762172b5caf5149e7aab408d5c96dc340d469
Navdeep Singh <me@navdeepsingh.ca>
2017-03-30 18:38:42 -0700

    Use hyperscript instead of h() in viewer.js Fix linting errors in viewer.js

--- desktop_www/js/viewer.js
+++ desktop_www/js/viewer.js
@@ -1,76 +1,78 @@
-const platform = require("./js/desktop_scripts");
-const h = require("../www/js/h");
+// eslint-disable-next-line import/no-unresolved
+const platform = require('./js/desktop_scripts');
+const h = require('hyperscript');
+
 const decks = [];
-let currentShabad,
-    currentLine;
-const $message  = document.getElementById("message");
-const $body     = document.body;
+let currentShabad;
+const $message = document.getElementById('message');
+const $body = document.body;
 
 $body.classList.add(process.platform);
 
-let prefs = platform.store.get("userPrefs.presenterWindow");
-applyPresenterPrefs(prefs);
-
-//IPC
-platform.ipc.on("show-line", function(event, data) {
-  const newShabadID = parseInt(data.shabadID);
-  if (decks.indexOf(newShabadID) > -1) {
-    const $shabadDeck = document.getElementById("shabad" + newShabadID);
-    if (currentShabad != newShabadID || !$shabadDeck.classList.contains("active")) {
-      hideDecks();
-      $shabadDeck.classList.add("active");
-      currentShabad = newShabadID;
-    }
-    Array.from($shabadDeck.querySelectorAll(".slide")).forEach(el => el.classList.remove("active"));
-    document.getElementById("slide" + data.lineID).classList.add("active");
-  } else {
-    let stmt = db.all("SELECT _id, gurmukhi, english_ssk, transliteration, sggs_darpan FROM shabad WHERE shabad_no = " + newShabadID,
-      (err, rows) => {
-        if (rows.length > 0) {
-          let cards = [];
-          rows.forEach((row, i) => {
-            cards.push(
-              h("div", { id: "slide" + row._id, class: "slide" + (row._id == data.lineID ? " active" : "") }, [
-                h("h1", { class: "gurbani gurmukhi" }, row.gurmukhi),
-                h("h2", { class: "translation" }, row.english_ssk),
-                h("h2", { class: "transliteration" }, row.transliteration),
-                h("h2", { class: "teeka" }, row.sggs_darpan)
-              ])
-            );
-          });
-          hideDecks();
-          $body.appendChild(h("div", { id: "shabad" + newShabadID, class: "deck active" }, cards));
-          currentShabad = parseInt(newShabadID);
-          decks.push(newShabadID);
-        }
-      }
-    );
-  }
-});
-
-platform.ipc.on("show-text", function(event, data) {
-  hideDecks();
-  $message.classList.add("active");
-  while ($message.firstChild) {
-    $message.removeChild($message.firstChild);
-  }
-  $message.appendChild(h("div", { class: "slide active" }, h("h1", { class: "gurmukhi gurbani" }, data.text)));
-});
-
 function hideDecks() {
-  Array.from(document.querySelectorAll(".deck")).forEach(el => {
-    el.classList.remove("active")
+  Array.from(document.querySelectorAll('.deck')).forEach((el) => {
+    el.classList.remove('active');
   });
+}
+
+function changeTheme(theme) {
+  $body.classList.forEach((bodyClass) => {
+    if (bodyClass.indexOf('theme') > -1) {
+      // $body.classList.remove(i);
+    }
+  });
+  $body.classList.add(theme);
 }
 
 function applyPresenterPrefs(prefs) {
   changeTheme(prefs.theme);
 }
-function changeTheme(theme) {
-  $body.classList.forEach(bodyClass => {
-    if (bodyClass.indexOf("theme") > -1) {
-      $body.classList.remove(i);
+
+const prefs = platform.store.get('userPrefs.presenterWindow');
+applyPresenterPrefs(prefs);
+
+// IPC
+platform.ipc.on('show-line', (event, data) => {
+  const newShabadID = parseInt(data.shabadID, 10);
+  if (decks.indexOf(newShabadID) > -1) {
+    const $shabadDeck = document.getElementById(`shabad${newShabadID}`);
+    if (currentShabad !== newShabadID || !$shabadDeck.classList.contains('active')) {
+      hideDecks();
+      $shabadDeck.classList.add('active');
+      currentShabad = newShabadID;
     }
-  });
-  $body.classList.add(theme);
-}
+    Array.from($shabadDeck.querySelectorAll('.slide')).forEach(el => el.classList.remove('active'));
+    document.getElementById(`slide${data.lineID}`).classList.add('active');
+  } else {
+    platform.db.all(`SELECT _id, gurmukhi, english_ssk, transliteration, sggs_darpan FROM shabad WHERE shabad_no = ${newShabadID}`,
+      (err, rows) => {
+        if (rows.length > 0) {
+          const cards = [];
+          rows.forEach((row) => {
+            cards.push(
+              h(
+                `div#slide${row._id}.slide${row._id === data.lineID ? '.active' : ''}`,
+                [
+                  h('h1.gurbani.gurmukhi', row.gurmukhi),
+                  h('h2.translation', row.english_ssk),
+                  h('h2.transliteration', row.transliteration),
+                  h('h2.teeka', row.sggs_darpan),
+                ]));
+          });
+          hideDecks();
+          $body.appendChild(h(`div#shabad${newShabadID}.deck.active`, cards));
+          currentShabad = parseInt(newShabadID, 10);
+          decks.push(newShabadID);
+        }
+      });
+  }
+});
+
+platform.ipc.on('show-text', (event, data) => {
+  hideDecks();
+  $message.classList.add('active');
+  while ($message.firstChild) {
+    $message.removeChild($message.firstChild);
+  }
+  $message.appendChild(h('div', { class: 'slide active' }, h('h1', { class: 'gurmukhi gurbani' }, data.text)));
+});
