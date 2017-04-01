115c1fb24774535bc57d695a9447ab57a704a3d8
Navdeep Singh <me@navdeepsingh.ca>
2017-03-31 17:54:13 -0700

    Update location for win32 auto-update Fix linting errors in app.js

--- app.js
+++ app.js
@@ -1,58 +1,75 @@
-const electron      = require("electron");
-const app           = require("app");
-const Menu          = electron.Menu;
+/* eslint import/no-extraneous-dependencies: 0, import/no-unresolved: 0, global-require:0 */
+const electron = require('electron');
+const app = require('app');
+if (require('electron-squirrel-startup')) app.quit();
+const appVersion = require('./package.json').version;
+const os = require('os').platform();
+const Store = require('./desktop_www/js/store.js');
+const defaultPrefs = require('./desktop_www/js/defaults.json');
+const autoUpdater = require('auto-updater');
+
+const Menu = electron.Menu;
 const BrowserWindow = electron.BrowserWindow;
-const ipc           = electron.ipcMain;
-const appVersion    = require("./package.json").version;
-const os            = require("os").platform();
-const Store         = require("./desktop_www/js/store.js");
-const defaultPrefs  = require("./desktop_www/js/defaults.json");
-let mainWindow,
-    viewerWindow,
-    viewerWindowOpen  = false,
-    viewerWindowX,
-    viewerWindowY,
-    viewerWindowFS;
+const ipc = electron.ipcMain;
+let mainWindow;
+let viewerWindow;
+let viewerWindowOpen = false;
+let viewerWindowX;
+let viewerWindowY;
+let viewerWindowFS;
 
 const store = new Store({
   configName: 'user-preferences',
-  defaults: defaultPrefs
+  defaults: defaultPrefs,
 });
 
-if (require("electron-squirrel-startup")) return;
-var autoUpdater = require("auto-updater");
-
-autoUpdater.on("update-available", () => {
-  mainWindow.webContents.send("updating");
+// autoUpdater events
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('updating');
 });
-autoUpdater.on("update-not-available", () => {
-  mainWindow.webContents.send("no-update");
+autoUpdater.on('update-not-available', () => {
+  mainWindow.webContents.send('no-update');
 });
-autoUpdater.on("update-downloaded", (e, releaseNotes, releaseName, releaseDate, updateURL) => {
-  mainWindow.webContents.send("updateReady");
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('updateReady');
 });
-autoUpdater.on("error", () => {
-  mainWindow.webContents.send("offline");
+autoUpdater.on('error', () => {
+  mainWindow.webContents.send('offline');
 });
 
-app.on("ready", function () {
-  let windowBounds = store.get("windowBounds");
+function checkForUpdates() {
+  if (process.env.NODE_ENV !== 'development') {
+    let updateFeed;
+    if (os === 'win32') {
+      updateFeed = 'http://sttm-releases.s3-website-us-west-2.amazonaws.com/win32/';
+    } else {
+      updateFeed = `http://releases.khalis.net/sttme/darwin/${appVersion}`;
+    }
+
+    autoUpdater.setFeedURL(updateFeed);
+    autoUpdater.checkForUpdates();
+    mainWindow.webContents.send('checkingForUpdates');
+  }
+}
+
+app.on('ready', () => {
+  const windowBounds = store.get('windowBounds');
   mainWindow = new BrowserWindow({
-    minWidth:       320,
-    minHeight:      480,
-    width:          windowBounds.width,
-    height:         windowBounds.height,
-    x:              windowBounds.x,
-    y:              windowBounds.y,
-    frame:          (process.platform == "win32" ? false : true),
-    show:           false,
-    titleBarStyle:  "hidden"
-  })
+    minWidth: 320,
+    minHeight: 480,
+    width: windowBounds.width,
+    height: windowBounds.height,
+    x: windowBounds.x,
+    y: windowBounds.y,
+    frame: (process.platform !== 'win32'),
+    show: false,
+    titleBarStyle: 'hidden',
+  });
   mainWindow.webContents.on('did-finish-load', () => {
     mainWindow.show();
     checkForUpdates();
-  })
-  mainWindow.loadURL("file://" + __dirname + "/www/index.html");
+  });
+  mainWindow.loadURL(`file://${__dirname}/www/index.html`);
   function saveWindowBounds() {
     store.set('windowBounds', mainWindow.getBounds());
   }
@@ -61,148 +78,148 @@ app.on("ready", function () {
   mainWindow.on('resize', saveWindowBounds);
   mainWindow.on('move', saveWindowBounds);
 
-  //Close all other windows if closing the main
-  mainWindow.on("close", () => {
+  // Close all other windows if closing the main
+  mainWindow.on('close', () => {
     if (viewerWindowOpen) {
       viewerWindow.close();
     }
   });
 
-  //macOS Menu
-  if (process.platform == "darwin") {
+  // macOS Menu
+  if (process.platform === 'darwin') {
     const template = [
       {
-        label: "SikhiToTheMax",
+        label: 'SikhiToTheMax',
         submenu: [
           {
             label: 'About SikhiToTheMax',
-            role: 'about'
+            role: 'about',
           },
           {
-            label: "Check for Updates...",
-            accelerator: "Cmd+U",
+            label: 'Check for Updates...',
+            accelerator: 'Cmd+U',
             click: () => {
               checkForUpdates();
-            }
+            },
           },
           {
-            type: 'separator'
+            type: 'separator',
           },
           {
-            label: "Preferences",
-            accelerator: "Cmd+,",
+            label: 'Preferences',
+            accelerator: 'Cmd+,',
             click: () => {
-              mainWindow.webContents.send("openSettings");
-            }
+              mainWindow.webContents.send('openSettings');
+            },
           },
           {
-            type: 'separator'
+            type: 'separator',
           },
           {
             label: 'Services',
             role: 'services',
-            submenu: []
+            submenu: [],
           },
           {
-            type: 'separator'
+            type: 'separator',
           },
           {
-            label: "Hide SikhiToTheMax",
-            accelerator: "Cmd+H",
-            role: 'hide'
+            label: 'Hide SikhiToTheMax',
+            accelerator: 'Cmd+H',
+            role: 'hide',
           },
           {
-            label: "Hide Others",
-            accelerator: "Cmd+Alt+H",
-            role: 'hideothers'
+            label: 'Hide Others',
+            accelerator: 'Cmd+Alt+H',
+            role: 'hideothers',
           },
           {
-            type: 'separator'
+            type: 'separator',
           },
           {
-            label: "Quit SikhiToTheMax",
-            accelerator: "CmdOrCtrl+Q",
+            label: 'Quit SikhiToTheMax',
+            accelerator: 'CmdOrCtrl+Q',
             click: () => {
               app.quit();
-            }
-          }
-        ]
+            },
+          },
+        ],
       },
       {
         label: 'Edit',
         submenu: [
           {
-            label: "Undo",
-            accelerator: "CmdOrCtrl+Z",
-            role: 'undo'
+            label: 'Undo',
+            accelerator: 'CmdOrCtrl+Z',
+            role: 'undo',
           },
           {
-            label: "Redo",
-            accelerator: "CmdOrCtrl+Shift+Z",
-            role: 'redo'
+            label: 'Redo',
+            accelerator: 'CmdOrCtrl+Shift+Z',
+            role: 'redo',
           },
           {
-            type: 'separator'
+            type: 'separator',
           },
           {
-            label: "Cut",
-            accelerator: "CmdOrCtrl+X",
-            role: 'cut'
+            label: 'Cut',
+            accelerator: 'CmdOrCtrl+X',
+            role: 'cut',
           },
           {
-            label: "Copy",
-            accelerator: "CmdOrCtrl+C",
-            role: 'copy'
+            label: 'Copy',
+            accelerator: 'CmdOrCtrl+C',
+            role: 'copy',
           },
           {
-            label: "Paste",
-            accelerator: "CmdOrCtrl+V",
-            role: 'paste'
+            label: 'Paste',
+            accelerator: 'CmdOrCtrl+V',
+            role: 'paste',
           },
           {
-            label: "Select All",
-            accelerator: "CmdOrCtrl+A",
-            role: 'selectall'
-          }
-        ]
+            label: 'Select All',
+            accelerator: 'CmdOrCtrl+A',
+            role: 'selectall',
+          },
+        ],
       },
       {
-        label: "Window",
+        label: 'Window',
         role: 'window',
         submenu: [
           {
-            label: "Minimize",
-            accelerator: "CmdOrCtrl+M",
-            role: 'minimize'
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize',
           },
           {
-            label: "Close",
-            accelerator: "CmdOrCtrl+W",
-            role: 'close'
-          }
-        ]
-      }
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close',
+          },
+        ],
+      },
     ];
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === 'development') {
       template.push({
-        label: "Dev",
+        label: 'Dev',
         submenu: [
           {
-            label: "Toggle Developer Tools",
-            accelerator: "CmdOrCtrl+Alt+I",
+            label: 'Toggle Developer Tools',
+            accelerator: 'CmdOrCtrl+Alt+I',
             click: () => {
               mainWindow.webContents.toggleDevTools();
-            }
+            },
           },
           {
-            label: "Reload",
-            accelerator: "CmdOrCtrl+R",
+            label: 'Reload',
+            accelerator: 'CmdOrCtrl+R',
             click: () => {
               mainWindow.webContents.reload();
-            }
-          }
-        ]
-      })
+            },
+          },
+        ],
+      });
     }
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
@@ -214,101 +231,87 @@ app.on("ready", function () {
 app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-  //if (process.platform !== 'darwin') {
-    app.quit();
-  //}
+  // if (process.platform !== 'darwin') {
+  app.quit();
+  // }
 });
+
+function checkForExternalDisplay() {
+  const electronScreen = electron.screen;
+  const displays = electronScreen.getAllDisplays();
+  let externalDisplay = null;
+  Object.keys(displays).forEach((i) => {
+    if (displays[i].bounds.x !== 0 || displays[i].bounds.y !== 0) {
+      externalDisplay = displays[i];
+    }
+  });
+
+  if (externalDisplay) {
+    viewerWindowX = externalDisplay.bounds.x + 50;
+    viewerWindowY = externalDisplay.bounds.y + 50;
+    viewerWindowFS = true;
+  } else {
+    viewerWindowX = 50;
+    viewerWindowY = 50;
+    viewerWindowFS = false;
+  }
+}
 
 function createViewer(ipcData) {
   checkForExternalDisplay();
   viewerWindow = new BrowserWindow({
-    width:            800,
-    height:           600,
-    x:                viewerWindowX,
-    y:                viewerWindowY,
-    fullscreen:       viewerWindowFS,
-    autoHideMenuBar:  true,
-    show:             false,
-    titleBarStyle:    "hidden",
-    frame:            (process.platform == "win32" ? false : true),
+    width: 800,
+    height: 600,
+    x: viewerWindowX,
+    y: viewerWindowY,
+    fullscreen: viewerWindowFS,
+    autoHideMenuBar: true,
+    show: false,
+    titleBarStyle: 'hidden',
+    frame: (process.platform !== 'win32'),
   });
-  viewerWindow.loadURL("file://" + __dirname + "/desktop_www/viewer.html");
-  viewerWindow.webContents.on("did-finish-load", () => {
+  viewerWindow.loadURL(`file://${__dirname}/desktop_www/viewer.html`);
+  viewerWindow.webContents.on('did-finish-load', () => {
     viewerWindow.show();
     mainWindow.focus();
     viewerWindowOpen = true;
-    if (typeof ipcData !== "undefined") {
+    if (typeof ipcData !== 'undefined') {
       viewerWindow.webContents.send(ipcData.send, ipcData.data);
     }
   });
-  viewerWindow.on("enter-full-screen", () => {
+  viewerWindow.on('enter-full-screen', () => {
     mainWindow.focus();
   });
-  viewerWindow.on("focus", () => {
-    //mainWindow.focus();
+  viewerWindow.on('focus', () => {
+    // mainWindow.focus();
   });
-  viewerWindow.on("closed", () => {
+  viewerWindow.on('closed', () => {
     viewerWindowOpen = false;
     viewerWindow = null;
   });
 }
 
-function checkForExternalDisplay() {
-  var electronScreen  = electron.screen;
-  var displays        = electronScreen.getAllDisplays();
-  var externalDisplay   = null;
-  for (var i in displays) {
-    if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
-      externalDisplay = displays[i];
-      break;
-    }
-  }
+ipc.on('checkForUpdates', checkForUpdates);
+ipc.on('quitAndInstall', () => autoUpdater.quitAndInstall());
 
-  if (externalDisplay) {
-    viewerWindowX   = externalDisplay.bounds.x + 50;
-    viewerWindowY   = externalDisplay.bounds.y + 50;
-    viewerWindowFS  = true;
-  } else {
-    viewerWindowX   = 50;
-    viewerWindowY   = 50;
-    viewerWindowFS  = false;
-  }
-}
-
-function checkForUpdates() {
-  if (process.env.NODE_ENV !== "development") {
-    let updateFeed = "http://releases.khalis.net/";
-    updateFeed += os === "win32" ?
-      "sttme-win32/" :
-      "sttme/darwin/" + appVersion;
-
-    autoUpdater.setFeedURL(updateFeed);
-    autoUpdater.checkForUpdates();
-    mainWindow.webContents.send("checkingForUpdates");
-  }
-}
-
-ipc.on("checkForUpdates", checkForUpdates);
-ipc.on("quitAndInstall", () => autoUpdater.quitAndInstall());
-
-ipc.on('show-line', function(event, arg) {
+ipc.on('show-line', (event, arg) => {
   if (viewerWindowOpen) {
-    viewerWindow.webContents.send("show-line", arg);
+    viewerWindow.webContents.send('show-line', arg);
   } else {
     createViewer({
-      send: "show-line",
-      data: arg
+      send: 'show-line',
+      data: arg,
     });
   }
 });
 
-ipc.on("show-text", function(event, arg) {
+ipc.on('show-text', (event, arg) => {
   if (viewerWindowOpen) {
-    viewerWindow.webContents.send("show-text", arg);
+    viewerWindow.webContents.send('show-text', arg);
   } else {
     createViewer({
-      send: "show-text",
-      data: arg
-    })
+      send: 'show-text',
+      data: arg,
+    });
   }
 });
