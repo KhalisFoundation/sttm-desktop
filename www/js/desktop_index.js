f7bf156025b60dc6ffa888a96617a0c785c64c9a
Navdeep Singh <me@navdeepsingh.ca>
2017-04-10 10:49:07 -0700

    Break out navigation into core Use a main JS file in the root and require core’s main JS Pull latest core HTML

---
+++ www/js/desktop_index.js
@@ -0,0 +1,12 @@
+// Node packages
+const fs = require('fs');
+const path = require('path');
+
+// Pull in navigator from core
+const navigator = fs.readFileSync(path.resolve(__dirname, 'core/navigator.html'));
+document.querySelector('#app-frame').innerHTML = navigator;
+
+const platform = require('./js/desktop_scripts');
+require('./core/js/index.js');
+
+document.body.classList.add(process.platform);
