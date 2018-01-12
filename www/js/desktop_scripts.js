2191059a446d236cccf73acf4d09cb05b6d6bda1
Navdeep Singh <me@navdeepsingh.ca>
2018-01-12 13:34:42 -0800

    Add progress to DB download on icon Fix #28

--- www/js/desktop_scripts.js
+++ www/js/desktop_scripts.js
@@ -74,13 +74,19 @@ module.exports = {
             if (force || curDBHash !== newestDBHash) {
               const dbZip = path.resolve(userDataPath, 'data.zip');
               progress(request('https://khajana.org/data.zip'))
-                .on('progress', state => global.core.search.updateDLProgress(state))
+                .on('progress', (state) => {
+                  const win = remote.getCurrentWindow();
+                  win.setProgressBar(state.percent);
+                  global.core.search.updateDLProgress(state);
+                })
                 .on('end', () => {
                   const zip = new AdmZip(dbZip);
                   zip.extractEntryTo('data.db', userDataPath, true, true);
                   module.exports.initDB();
                   module.exports.setPref('curDBHash', newestDBHash);
                   fs.unlinkSync(dbZip);
+                  const win = remote.getCurrentWindow();
+                  win.setProgressBar(-1);
                 })
                 .pipe(fs.createWriteStream(dbZip));
             }
