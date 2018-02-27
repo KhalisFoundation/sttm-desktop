8e79582097906c2d916aa928b4f3a534a1e05a4a
Harkeerat Toor <hadou798@gmail.com>
2018-02-27 07:07:11 -0800

    Init commit of chromecast implementation

--- www/js/viewer.js
+++ www/js/viewer.js
@@ -77,23 +77,23 @@ module.exports = {
       if (!global.platform.db) {
         global.platform.initDB();
       }
-          global.platform.db.all(`SELECT v.ID, v.Gurmukhi, v.English, v.transliteration, v.PunjabiUni FROM Verse v LEFT JOIN Shabad s ON v.ID = s.VerseID WHERE s.ShabadID = ${newShabadID} ORDER BY v.ID ASC`,
+      global.platform.db.all(`SELECT v.ID, v.Gurmukhi, v.English, v.transliteration, v.PunjabiUni FROM Verse v LEFT JOIN Shabad s ON v.ID = s.VerseID WHERE s.ShabadID = ${newShabadID} ORDER BY v.ID ASC`,
         (err, rows) => {
-        if (rows.length > 0) {
-          const cards = [];
-          rows.forEach((row) => {
-            const gurmukhiShabads = row.Gurmukhi.split(' ');
-            const taggedGurmukhi = [];
-            gurmukhiShabads.forEach((val, index) => {
-              if (val.indexOf(']') !== -1) {
-                taggedGurmukhi[index - 1] = `<span>${taggedGurmukhi[index - 1]}<i> </i>${val}</span>`;
-              } else {
-                taggedGurmukhi[index] = val;
-              }
-            });
-            const gurmukhiContainer = document.createElement('div');
-            gurmukhiContainer.innerHTML = `<span class="padchhed">${taggedGurmukhi.join(' ')}</span><span class="larivaar">${taggedGurmukhi.join('<wbr>')}</span>`;
-            cards.push(
+          if (rows.length > 0) {
+            const cards = [];
+            rows.forEach((row) => {
+              const gurmukhiShabads = row.Gurmukhi.split(' ');
+              const taggedGurmukhi = [];
+              gurmukhiShabads.forEach((val, index) => {
+                if (val.indexOf(']') !== -1) {
+                  taggedGurmukhi[index - 1] = `<span>${taggedGurmukhi[index - 1]}<i> </i>${val}</span>`;
+                } else {
+                  taggedGurmukhi[index] = val;
+                }
+              });
+              const gurmukhiContainer = document.createElement('div');
+              gurmukhiContainer.innerHTML = `<span class="padchhed">${taggedGurmukhi.join(' ')}</span><span class="larivaar">${taggedGurmukhi.join('<wbr>')}</span>`;
+              cards.push(
               h(
                   `div#slide${row.ID}.slide${row.ID === lineID ? '.active' : ''}`,
                 [
@@ -102,14 +102,16 @@ module.exports = {
                   h('h2.teeka', row.PunjabiUni),
                   h('h2.transliteration', row.Transliteration),
                 ]));
-          });
-          hideDecks();
-          $viewer.appendChild(h(`div#shabad${newShabadID}.deck.active`, cards));
-          currentShabad = parseInt(newShabadID, 10);
+              sendMessage(row.Gurmukhi);
+            });
+            hideDecks();
+            $viewer.appendChild(h(`div#shabad${newShabadID}.deck.active`, cards));
+            currentShabad = parseInt(newShabadID, 10);
             decks.push(newShabadID);
-      }
+          }
         });
     }
+    //sendMessage(newShabadID);
   },
 
   showText(text, isGurmukhi = false) {
