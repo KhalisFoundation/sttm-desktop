891da1dfd8e67f151236391fd694a2724c41b61a
Navdeep Singh <me@navdeepsingh.ca>
2018-04-11 19:20:36 -0700

    Bring navigator HTML into index

--- www/js/desktop_index.js
+++ www/js/desktop_index.js
@@ -1,16 +1,9 @@
 /* eslint import/no-unresolved: 0 */
-global.electron = true;
-global.mainWindow = true;
-
-const fs = require('fs');
-const path = require('path');
 global.platform = require('./js/desktop_scripts');
 global.controller = require('./js/controller');
 global.core = require('./js/index');
 
 // Pull in navigator from core
-const navigator = fs.readFileSync(path.resolve(__dirname, 'navigator.html'));
-document.querySelector('#navigator').innerHTML = navigator;
 global.core.search.init();
 global.core.menu.init();
 global.platform.init();
