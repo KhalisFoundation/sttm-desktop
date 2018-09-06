26e4a80b8664a72d551eada939a2d43af23534b5
Navdeep Singh <me@navdeepsingh.ca>
2018-09-05 17:49:32 -0700

    Update Realm schemas

--- www/js/realm-db.js
+++ www/js/realm-db.js
@@ -7,6 +7,7 @@ const realmPath = path.resolve(userDataPath, 'sttmdesktop.realm');
 
 const RaagSchema = {
   name: 'Raag',
+  primaryKey: 'RaagID',
   properties: {
     RaagID: 'int',
     RaagGurmukhi: 'string?',
@@ -18,11 +19,23 @@ const RaagSchema = {
   },
 };
 
+const ShabadSchema = {
+  name: 'Shabad',
+  primaryKey: 'ShabadID',
+  properties: {
+    ShabadID: 'int',
+  },
+};
+
 const SourceSchema = {
   name: 'Source',
+  primaryKey: 'UniqueID',
   properties: {
     UniqueID: 'int',
-    SourceID: 'string',
+    SourceID: {
+      type: 'string',
+      indexed: true,
+    },
     SourceGurmukhi: 'string?',
     SourceUnicode: 'string?',
     SourceEnglish: 'string?',
@@ -31,34 +44,49 @@ const SourceSchema = {
 
 const VerseSchema = {
   name: 'Verse',
+  primaryKey: 'ID',
   properties: {
     ID: 'int',
     English: 'string?',
     Gurmukhi: 'string?',
     GurmukhiBisram: 'string?',
     GurmukhiUni: 'string?',
-    WriterID: 'int?',
+    Writer: 'Writer?',
     Punjabi: 'string?',
     PunjabiUni: 'string?',
     Spanish: 'string?',
-    RaagID: 'int?',
-    PageNo: 'int?',
+    Raag: 'Raag?',
+    PageNo: {
+      type: 'int?',
+      indexed: true,
+    },
     LineNo: 'int?',
-    SourceID: 'string',
-    FirstLetterStr: 'string?',
+    SourceID: 'Source',
+    FirstLetterStr: {
+      type: 'string?',
+      indexed: true,
+    },
     MainLetters: 'string?',
     Bisram: 'string?',
     igurbani_bisram1: 'string?',
     igurbani_bisram2: 'string?',
-    FirstLetterEng: 'string?',
+    FirstLetterEng: {
+      type: 'string?',
+      indexed: true,
+    },
     Transliteration: 'string?',
-    Updated: 'string',
-    FirstLetterLen: 'int',
+    Updated: 'date',
+    FirstLetterLen: {
+      type: 'int?',
+      indexed: true,
+    },
+    Shabads: 'Shabad[]',
   },
 };
 
 const WriterSchema = {
   name: 'Writer',
+  primaryKey: 'WriterID',
   properties: {
     WriterID: 'int',
     WriterEnglish: 'string?',
@@ -70,11 +98,6 @@ const WriterSchema = {
 module.exports = {
   realmVerseSchema: {
     path: realmPath,
-    schema: [VerseSchema],
-  },
-
-  realmMetaSchema: {
-    path: realmPath,
-    schema: [RaagSchema, SourceSchema, WriterSchema],
+    schema: [RaagSchema, ShabadSchema, SourceSchema, VerseSchema, WriterSchema],
   },
 };
