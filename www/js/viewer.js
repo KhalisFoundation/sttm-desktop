4945b38fa2b917c0f6e76a01069610698565341f
Navdeep Singh <me@navdeepsingh.ca>
2019-01-15 19:04:48 -0800

    Revert GurmukhiBisram usage (#446)
    * Revert GurmukhiBisram usage
    
    Commenting out until database is updated with better data
    
    * Update viewer.js

--- www/js/viewer.js
+++ www/js/viewer.js
@@ -218,17 +218,18 @@ const createCards = (rows, LineID) => {
   Object.keys(rows).forEach((key) => {
     row = rows[key];
     lines.push(row.ID);
-    const gurmukhiShabads = row.GurmukhiBisram.split(' ');
+    // const gurmukhiShabads = row.GurmukhiBisram.split(' ');
+    const gurmukhiShabads = row.Gurmukhi.split(' ');
     const taggedGurmukhi = [];
     gurmukhiShabads.forEach((val, index) => {
       if (val.indexOf(']') !== -1) {
         taggedGurmukhi[index - 1] = `<span>${taggedGurmukhi[index - 1]}<i> </i>${val}</span>`;
-      } else if (val.includes(';')) {
+      /* } else if (val.includes(';')) {
         const bisramWord = val.slice(0, -1);
         taggedGurmukhi[index] = `<span class="bisram-main">${bisramWord}</span>`;
       } else if (val.includes(',')) {
         const yamkiWord = val.slice(0, -1);
-        taggedGurmukhi[index] = `<span class="bisram-yamki">${yamkiWord}</span>`;
+        taggedGurmukhi[index] = `<span class="bisram-yamki">${yamkiWord}</span>`; */
       } else {
         taggedGurmukhi[index] = val;
       }
@@ -236,7 +237,7 @@ const createCards = (rows, LineID) => {
     const gurmukhiContainer = document.createElement('div');
 
     gurmukhiContainer.innerHTML = `<span class="padchhed">${taggedGurmukhi.join(' ')}</span>
-                                    <span class="larivaar">${taggedGurmukhi.join('<wbr>')} </span>`;
+                                    <span class="larivaar">${taggedGurmukhi.join('<wbr>')}</span>`;
     cards.push(
       h(
         `div#slide${row.ID}.slide${row.ID === LineID ? '.active' : ''}`,
@@ -247,7 +248,7 @@ const createCards = (rows, LineID) => {
           h('h2.transliteration', row.Transliteration),
         ]));
     shabad[row.ID] = {
-      gurmukhi: row.GurmukhiBisram,
+      gurmukhi: row.Gurmukhi,
       gurmukhiWithoutBisram: row.Gurmukhi,
       larivaar: taggedGurmukhi.join('<wbr>'),
       translation: row.English,
