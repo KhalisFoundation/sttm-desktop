eb3ca1bc397463042e04b033bb1905d56dca58ae
Navdeep Singh <me@navdeepsingh.ca>
2019-01-17 15:56:06 -0800

    Don't set remote and adjust logic for PRs

--- packaging/mac.sh
+++ packaging/mac.sh
@@ -2,19 +2,16 @@
 # exit when any command fails
 set -ev
 
-echo $TRAVIS_BRANCH
 if [ "$TRAVIS_BRANCH" = "release" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "dev" ]; then
   GIT_TAG=$(git describe --abbrev=0 2>&1)
   RELEASE_VERSION=$(node packaging/check-version.js $TRAVIS_BRANCH $GIT_TAG)
   npm test
   npm run dist:mac
-  git remote add origin https://$GH_TOKEN@github.com/khalisfoundation/sttm-desktop.git
+  # git remote add origin https://$GH_TOKEN@github.com/khalisfoundation/sttm-desktop.git
   git tag $RELEASE_VERSION
   git push --tags
   if [ "$TRAVIS_BRANCH" = "release" ]; then
     echo "should not echo"
     # node update-mac.js $TRAVIS_BRANCH
   fi
-else
-  npm test;
 fi
