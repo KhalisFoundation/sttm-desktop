d42ecc79a9c38c4e310cda06d97ef0d5e70732b3
Navdeep Singh <me@navdeepsingh.ca>
2019-01-18 06:38:48 -0800

    Try adding user config

--- packaging/mac.sh
+++ packaging/mac.sh
@@ -7,10 +7,12 @@ if [ "$TRAVIS_BRANCH" = "release" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TR
   RELEASE_VERSION=$(node packaging/check-version.js $TRAVIS_BRANCH $GIT_TAG)
   npm test
   npm run dist:mac
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git config --global push.default current
+  git checkout ${TRAVIS_BRANCH}
   git tag $RELEASE_VERSION
-  git remote rm origin
-  git remote add origin https://nsingh:$GH_TOKEN@github.com/khalisfoundation/sttm-desktop.git
-  git push --tags
+  git push https://${GH_TOKEN}@github.com/stoplay/stoplay-ext.git --tags
   if [ "$TRAVIS_BRANCH" = "release" ]; then
     echo "should not echo"
     # node update-mac.js $TRAVIS_BRANCH
