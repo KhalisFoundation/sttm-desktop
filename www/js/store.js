2da128be9a785981d734f7ce000b94838c170fe0
tsingh777 <tsingh777@gmail.com>
2019-02-12 19:48:50 -0800

    Update store.js

--- www/js/store.js
+++ www/js/store.js
@@ -10,7 +10,7 @@ function parseDataFile(filePath, defaults) {
   // which will be the case on the first application run.
   // `fs.readFileSync` will return a JSON string which we then parse into a Javascript object
   try {
-    return JSON.parse(fs.readFileSync(filePath, { flag: 'w' }));
+    return JSON.parse(fs.readFileSync(filePath, { flag: 'w+' }));
   } catch (error) {
     // if there was some kind of error, return the passed in defaults instead.
     return defaults;
