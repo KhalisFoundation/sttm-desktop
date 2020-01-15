0d2e39e609b4608c50240b24eaca78aad2803e47
inderpreetsingh <singhs.ishwer@gmail.com>
2020-01-15 18:39:44 +0530

    add comments where needed

--- www/js/toolbar.js
+++ www/js/toolbar.js
@@ -54,12 +54,16 @@ const setListeners = () => {
       const { search } = global.core.search;
       const isPinCorrect = parseInt(data.pin, 10) === adminPin;
 
+      /* We need gurmukhi here to add for history support.
+      Will no longer be needed when we move to better state management */
       const loadShabad = (shabadId, verseId, gurmukhi) => {
         const currentShabadID = search.getCurrentShabadID();
         const currentVerse = document.querySelector(`#line${verseId}`);
+        // If its not new shabad but just a verse change in current shabad
         if (currentShabadID === shabadId && currentVerse) {
           currentVerse.click();
         } else {
+          // if its a new shabad load it and add it to history
           global.core.search.loadShabad(shabadId, verseId);
           search.addToHistory(shabadId, verseId, gurmukhi);
         }
