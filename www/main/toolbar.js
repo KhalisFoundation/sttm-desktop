0f8e8b16c65a734612eee78a67700f1d9ef6588f
inderpreetsingh <singhs.ishwer@gmail.com>
2020-08-03 19:36:45 +0530

    send settings on connect

--- www/main/toolbar.js
+++ www/main/toolbar.js
@@ -131,6 +131,9 @@ const setListeners = () => {
             host: 'sttm-desktop',
             type: 'response-control',
             success: isPinCorrect,
+            settings: {
+              fontSizes: store.getUserPref('slide-layout.font-sizes'),
+            },
           });
 
           // if Pin is correct and there is a shabad already in desktop, emit that shabad details.
