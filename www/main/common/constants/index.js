5a953fc111bd982ad39a5a3386ec306cbf4243bd
inderpreetsingh <singhs.ishwer@gmail.com>
2020-10-26 18:57:35 +0530

    removes unnecessary constant

--- www/main/common/constants/index.js
+++ www/main/common/constants/index.js
@@ -4,7 +4,6 @@ export { default as globalInit } from './global-init';
 export const DEFAULT_OVERLAY = 'none';
 export const nitnemBaniIds = [2, 4, 6, 9, 10, 20, 21, 23];
 export const popularBaniIds = [90, 30, 31, 22];
-export const visibleCeremoniesIds = [1, 3, 5];
 export const ceremoniesFilter = {
   visible: [1, 3, 5],
   englishToggle: [1],
