09eefd64930a575fa7c47cec1778e9127342cc1e
gauravjeetsingh <gaurav.ishwerdas@gmail.com>
2020-11-04 22:18:39 +0530

    Fixes linting errors

--- www/main/settings/utils/generate-markup.js
+++ www/main/settings/utils/generate-markup.js
@@ -16,90 +16,92 @@ const generateMarkup = (type, options, titleKey, settingsKey, control) => {
     const dropdownOptions = options[name].options;
     const { max, min, step, checkbox } = options[name];
 
-    let savedValue, checkBoxValue;
+    let savedValue;
+    let checkBoxValue;
 
     try {
       savedValue = userPrefs[settingsKey][control][name];
       if (checkbox) {
-        checkBoxValue = userPrefs[settingsKey]['fields'][checkbox];
-        console.log('checkbox for' + settingsKey + ' ' + checkBoxValue);
+        checkBoxValue = userPrefs[settingsKey].fields[checkbox];
+        console.log('checkbox for', settingsKey, checkBoxValue);
       }
     } catch (e) {
       console.log('for default');
       console.log(settingsKey, control, name, checkbox);
       savedValue = defaultPrefs[settingsKey][control][name];
       if (checkbox) {
-        checkBoxValue = defaultPrefs[settingsKey]['fields'][checkbox];
+        checkBoxValue = defaultPrefs[settingsKey].fields[checkbox];
       }
     }
 
-    if (type == 'range') {
-      return (
-        <div
-          key={`control-item-range-${index}`}
-          className={`control-item ${name}-range`}
-          id={`${name}-range-id`}
-        >
-          {checkbox ? (
-            <Checkbox
-              id={checkbox}
-              name={checkbox}
-              value={checkbox}
-              checked={checkBoxValue}
-              label={i18n.t(`${titleKey}${controlname}`)}
-              handler={() => {
-                console.log('clicked the checkbox');
-              }}
-            />
-          ) : (
-            <span>{i18n.t(`${titleKey}${controlname}`)}</span>
-          )}
-          <input
-            type="range"
-            data-value={savedValue}
-            min={min}
-            max={max}
-            step={step}
-            value={savedValue}
-          ></input>
-        </div>
-      );
-    } else if (type === 'dropdown') {
-      return (
-        <div
-          key={`control-item-dropdown-${index}`}
-          className={`control-item ${name}`}
-          id={`${name}-switch`}
-        >
-          <span>{i18n.t(`${titleKey}${controlname}`)}</span>
-          <select
-            value={savedValue}
-            onChange={() => {
-              console.log('changed the dropdown');
-            }}
+    switch (type) {
+      case 'range':
+        return (
+          <div
+            key={`control-item-range-${index}`}
+            className={`control-item ${name}-range`}
+            id={`${name}-range-id`}
           >
-            {Object.keys(dropdownOptions).map((op, opIndex) => (
-              <option key={`control-dropdown-options-${opIndex}`}>
-                {i18n.t(`${titleKey}${dropdownOptions[op]}`)}
-              </option>
-            ))}
-          </select>
-        </div>
-      );
-    } else if (type === 'switch') {
-      controlname = typeof options[name] === 'object' ? options[name].label : options[name];
-      return (
-        <Switch
-          key={`control-item-switch-${index}`}
-          title={i18n.t(`${titleKey}${controlname}`)}
-          controlId={`${name}-switch`}
-          className={`control-item ${name}`}
-          value={savedValue}
-          onToggle={() => {
-            console.log('switched the switch');
-          }}
-        />
-      );
+            {checkbox ? (
+              <Checkbox
+                id={checkbox}
+                name={checkbox}
+                value={checkbox}
+                checked={checkBoxValue}
+                label={i18n.t(`${titleKey}${controlname}`)}
+                handler={() => {
+                  console.log('clicked the checkbox');
+                }}
+              />
+            ) : (
+              <span>{i18n.t(`${titleKey}${controlname}`)}</span>
+            )}
+            <input
+              type="range"
+              data-value={savedValue}
+              min={min}
+              max={max}
+              step={step}
+              value={savedValue}
+            ></input>
+          </div>
+        );
+      case 'dropdown':
+        return (
+          <div
+            key={`control-item-dropdown-${index}`}
+            className={`control-item ${name}`}
+            id={`${name}-switch`}
+          >
+            <span>{i18n.t(`${titleKey}${controlname}`)}</span>
+            <select
+              value={savedValue}
+              onChange={() => {
+                console.log('changed the dropdown');
+              }}
+            >
+              {Object.keys(dropdownOptions).map((op, opIndex) => (
+                <option key={`control-dropdown-options-${opIndex}`}>
+                  {i18n.t(`${titleKey}${dropdownOptions[op]}`)}
+                </option>
+              ))}
+            </select>
+          </div>
+        );
+      case 'switch':
+        controlname = typeof options[name] === 'object' ? options[name].label : options[name];
+        return (
+          <Switch
+            key={`control-item-switch-${index}`}
+            title={i18n.t(`${titleKey}${controlname}`)}
+            controlId={`${name}-switch`}
+            className={`control-item ${name}`}
+            value={savedValue}
+            onToggle={() => {
+              console.log('switched the switch');
+            }}
+          />
+        );
     }
   });
   return items;
