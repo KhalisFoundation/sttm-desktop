4c54304e9c509df318abf25b2b04d87130c9f3dc
inderpreetsingh <singhs.ishwer@gmail.com>
2020-11-05 16:03:19 +0530

    adds workspace names to translations

--- www/main/workspace-bar/components/WorkspaceBar.jsx
+++ www/main/workspace-bar/components/WorkspaceBar.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 import { remote } from 'electron';
 
+const { i18n } = remote.require('./app');
+const analytics = remote.getGlobal('analytics');
+
 const { store } = remote.require('./app');
 
 const WorkspaceBar = () => {
-  const workspaces = ['Presenter', 'Single Screen'];
+  const presenterIdentifier = i18n.t('WORKSPACES.PRESENTER');
+  const workspaces = [presenterIdentifier, i18n.t('WORKSPACES.SINGLE_DISPLAY')];
   const defaultWsState = store.getUserPref('app.layout.presenter-view')
     ? workspaces[0]
     : workspaces[1];
   const [currentWorkspace, setWorkspace] = useState(defaultWsState);
 
   const handleWorkspaceChange = workspace => {
-    const moveToPresenter = workspace === 'Presenter';
+    const moveToPresenter = workspace === presenterIdentifier;
     store.setUserPref('app.layout.presenter-view', moveToPresenter);
     global.platform.updateSettings();
     global.controller['presenter-view']();
     setWorkspace(workspace);
+    analytics.trackEvent('changed workspace', workspace);
   };
 
   return (
