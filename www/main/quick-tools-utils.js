6dc4c57d291ac1113bd23003098b388d52eee36d
inderpreetsingh <singhs.ishwer@gmail.com>
2020-12-01 20:17:06 +0530

    adds radix parameter

--- www/main/quick-tools-utils.js
+++ www/main/quick-tools-utils.js
@@ -3,7 +3,7 @@ const firstCharToUpperCase = str => `${str.charAt(0).toUpperCase()}${str.slice(1
 export const changeFontSize = (iconType, increase = true) => {
   const setterAction = `set${firstCharToUpperCase(iconType)}FontSize`;
   const getterVar = `${iconType}FontSize`;
-  const oldValue = parseInt(global.getUserSettings[getterVar]);
+  const oldValue = parseInt(global.getUserSettings[getterVar], 10);
   const newValue = increase ? oldValue + 1 : oldValue - 1;
   global.setUserSettings[setterAction](newValue);
 };
