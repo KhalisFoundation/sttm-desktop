32ea4a4171439728fbfad118a042746b6862798a
inderpreetsingh <singhs.ishwer@gmail.com>
2020-12-15 16:09:47 +0530

    move to lodash.camelCase

--- www/main/common/utils/convert-to-camel-case.js
+++ www/main/common/utils/convert-to-camel-case.js
@@ -1,8 +1,10 @@
+const camelCase = require('lodash.camelcase');
+
 const convertToCamelCase = (str, isPascalCase = false) => {
-  const camelCase = str.split('-').reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1));
+  const camelCaseStr = camelCase(str);
   const finalString = isPascalCase
-    ? camelCase.charAt(0).toUpperCase() + camelCase.slice(1)
-    : camelCase;
+    ? camelCaseStr.charAt(0).toUpperCase() + camelCaseStr.slice(1)
+    : camelCaseStr;
   return finalString;
 };
 
