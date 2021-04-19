6ad45215174d4801e6415c11146756726e35c3f8
inderpreetsingh <singhs.ishwer@gmail.com>
2021-04-19 21:24:39 +0530

    fixes size of boxes

--- www/main/navigator/viewer/ViewerPane.jsx
+++ www/main/navigator/viewer/ViewerPane.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// import Pane from '../../common/sttm-ui/pane/Pane';
+import Pane from '../../common/sttm-ui/pane/Pane';
 import ViewerContent from './ViewerContent';
 
 function ViewerPane() {
   return (
     <div className="viewer-pane">
-      <ViewerContent />
+      <Pane header={null} content={ViewerContent} footer={null} />
     </div>
   );
 }
