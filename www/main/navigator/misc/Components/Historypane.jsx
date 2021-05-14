12b7ada2c0d61efc3f772efcb7c569f890aff25f
inderpreetsingh <singhs.ishwer@gmail.com>
2021-05-14 20:07:05 +0530

    fixes versepanel issue in historypane

--- www/main/navigator/misc/Components/Historypane.jsx
+++ www/main/navigator/misc/Components/Historypane.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useStoreState } from 'easy-peasy';
-import VersePanel from '../../../common/sttm-ui/versepanel/VersePanel';
+// import { useStoreState } from 'easy-peasy';
+// import VersePanel from '../../../common/sttm-ui/versepanel/VersePanel';
 
 function Historypane() {
-  const versesHistory = useStoreState(state => state.navigator.versesHistory);
-  const activeVerse = useStoreState(state => state.navigator.selectedVerse);
+  // const versesHistory = useStoreState(state => state.navigator.versesHistory);
+  // const activeVerse = useStoreState(state => state.navigator.selectedVerse);
 
   return (
     <div className="history-results">
-      <VersePanel verses={versesHistory} HistoryPane activeVerse={activeVerse} />
+      {/* <VersePanel verses={versesHistory} HistoryPane activeVerse={activeVerse} /> */}
     </div>
   );
 }
