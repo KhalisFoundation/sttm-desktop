d7657bdf7573f3ec4426cb603a7ff3752e58bf57
Gauravjeetsingh <gaurav.ishwerdas@gmail.com>
2021-07-21 20:25:14 +0530

    Fixes the theme preview on windows

--- www/main/settings/utils/theme-bg-reader.js
+++ www/main/settings/utils/theme-bg-reader.js
@@ -7,6 +7,7 @@ const util = require('util');
 const mkdir = util.promisify(fs.mkdir);
 const userDataPath = remote.app.getPath('userData');
 const userBackgroundsPath = path.resolve(userDataPath, 'user_backgrounds');
+const userBackgroundsURL = new URL(`file:///${userBackgroundsPath}`).href;
 
 const errorAlert = error => {
   alert(error);
@@ -27,7 +28,7 @@ export const upsertCustomBackgrounds = (responseCallback = () => {}) => {
         files
           .map(file => ({
             name: file,
-            'background-image': `${userBackgroundsPath}/${file.replace(/(\s)/g, '\\ ')}`,
+            'background-image': `${userBackgroundsURL}/${file.replace(/(\s)/g, '\\ ')}`,
             time: fs.statSync(path.resolve(userBackgroundsPath, file)).mtime.getTime(),
           }))
           .sort((a, b) => b.time - a.time),
