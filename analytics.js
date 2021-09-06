3d7b2e62ef46661600950169c4d3579c9e0a01ea
inderpreetsingh <singhs.ishwer@gmail.com>
2021-09-06 22:09:07 +0530

    fix: tracking issue due to electron store

--- analytics.js
+++ analytics.js
@@ -32,19 +32,25 @@ class Analytics {
     const useragent = this.store.get('user-agent');
 
     if (process.env.NODE_ENV !== 'development') {
-      if (this.store.get('userPrefs.app.analytics.collectStatistics')) {
+      // TODO: need to add variable that stops statistics collection
+      if (true) {
         isOnline().then(online => {
           // TODO: for offline users, come up with a way of storing and send when online.
           if (online && this.usr) {
             this.usr
-              .event({
-                ec: category,
-                ea: action,
-                el: label,
-                ev: value,
-                ua: useragent,
-                cd1: appVersion,
-              })
+              .event(
+                {
+                  ec: category,
+                  ea: action,
+                  el: label,
+                  ev: value,
+                  ua: useragent,
+                  cd1: appVersion,
+                },
+                err => {
+                  console.log(err);
+                },
+              )
               .send();
           }
         });
