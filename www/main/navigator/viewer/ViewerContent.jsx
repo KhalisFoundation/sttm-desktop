0a736f2eb4b3296bd3b95ce7065270cc47b7f5c2
inderpreetsingh <singhs.ishwer@gmail.com>
2021-09-15 00:59:20 +0530

    fix: convert es5 functions to es6

--- www/main/navigator/viewer/ViewerContent.jsx
+++ www/main/navigator/viewer/ViewerContent.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function ViewerContent() {
+const ViewerContent = () => {
   return (
     <div className="viewer-content">
       <webview id="webview-viewer" nodeintegration="true" src="viewer.html" className="base-ui" />
     </div>
   );
-}
+};
 
 export default ViewerContent;
