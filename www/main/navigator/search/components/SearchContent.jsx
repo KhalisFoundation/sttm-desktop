7111dc2c1642c7f3bf90077d8ef636cfd712945d
Gauravjeetsingh <gaurav.ishwerdas@gmail.com>
2021-11-18 19:12:09 +0530

    Fix history issue when switching between shabad, bani and ceremony

--- www/main/navigator/search/components/SearchContent.jsx
+++ www/main/navigator/search/components/SearchContent.jsx
@@ -67,8 +67,8 @@ const SearchContent = () => {
   const openFirstResult = () => {
     if (searchQuery.length > 0 && filteredShabads.length > 0) {
       // Takes { shabadId, verseId, verse } from the first shabad in search result
-      const { shabadId, verseId, verse } = filteredShabads[0];
-      changeActiveShabad(shabadId, verseId, verse);
+      const { shabadId, verseId } = filteredShabads[0];
+      changeActiveShabad(shabadId, verseId);
     }
   };
 
--- www/main/navigator/search/hooks/use-new-shabad.jsx
+++ www/main/navigator/search/hooks/use-new-shabad.jsx
@@ -2,73 +2,32 @@ import { useStoreActions, useStoreState } from 'easy-peasy';
 
 export const useNewShabad = () => {
   const {
-    verseHistory,
     versesRead,
-    isEmptySlide,
-    isWaheguruSlide,
     activeShabadId,
     initialVerseId,
     activeVerseId,
-    isAnnouncementSlide,
-    isMoolMantraSlide,
-    isDhanGuruSlide,
     isSundarGutkaBani,
     isCeremonyBani,
+    isMiscSlide,
   } = useStoreState(state => state.navigator);
 
   const {
     setActiveShabadId,
     setInitialVerseId,
-    setVerseHistory,
     setVersesRead,
     setActiveVerseId,
-    setIsEmptySlide,
-    setIsWaheguruSlide,
-    setIsMoolMantraSlide,
-    setIsAnnouncementSlide,
-    setIsDhanGuruSlide,
+    setIsMiscSlide,
     setIsSundarGutkaBani,
     setIsCeremonyBani,
   } = useStoreActions(state => state.navigator);
 
-  return (newSelectedShabad, newSelectedVerse, newVerse = '') => {
-    const check = verseHistory.filter(historyObj => historyObj.shabadId === newSelectedShabad);
-    if (check.length === 0) {
-      const updatedHistory = [
-        ...verseHistory,
-        {
-          shabadId: newSelectedShabad,
-          verseId: newSelectedVerse,
-          label: newVerse,
-          type: 'shabad',
-          meta: {
-            baniLength: '',
-          },
-          versesRead: [newSelectedVerse],
-          continueFrom: newSelectedVerse,
-          homeVerse: 0,
-        },
-      ];
-      setVerseHistory(updatedHistory);
-    }
+  return (newSelectedShabad, newSelectedVerse) => {
     // Push verseId of active Verse to versesRead Array when shabad is changed
     if (!versesRead.includes(newSelectedVerse)) {
       setVersesRead([newSelectedVerse]);
     }
-    if (isWaheguruSlide) {
-      setIsWaheguruSlide(false);
-    }
-    if (isAnnouncementSlide) {
-      setIsAnnouncementSlide(false);
-    }
-    if (isEmptySlide) {
-      setIsEmptySlide(false);
-    }
-    if (isMoolMantraSlide) {
-      setIsMoolMantraSlide(false);
-    }
-    if (isDhanGuruSlide) {
-      setIsDhanGuruSlide(false);
+    if (isMiscSlide) {
+      setIsMiscSlide(false);
     }
     if (isSundarGutkaBani) {
       setIsSundarGutkaBani(false);
