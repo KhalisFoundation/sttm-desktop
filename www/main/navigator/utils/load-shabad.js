93b3eed958eb49e0ad111168010d0e7e1fb940e9
Inderpreet <singhs.ishwer@gmail.com>
2021-12-28 21:51:40 +0530

    fix: first shabad not showing issue

--- www/main/navigator/utils/load-shabad.js
+++ www/main/navigator/utils/load-shabad.js
@@ -1,5 +1,10 @@
 import banidb from '../../banidb';
 
 export const loadShabad = (shabadID, lineID) => {
-  return banidb.loadShabad(shabadID, lineID).then(rows => rows);
+  return banidb
+    .loadShabad(shabadID, lineID)
+    .then(rows => rows)
+    .catch(() => {
+      console.log('err while loading shabad, please contact dev team');
+    });
 };
