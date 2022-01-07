e58a46450e034aad81a6db01618ef7516f819370
Inderpreet <singhs.ishwer@gmail.com>
2022-01-07 16:08:58 +0530

    fix(themes): unable to delete image issue

--- www/main/settings/components/ThemeContainer.jsx
+++ www/main/settings/components/ThemeContainer.jsx
@@ -78,7 +78,7 @@ const ThemeContainer = () => {
                   applyTheme(tile, 'custom', setTheme, setThemeBg);
                 }}
                 onRemove={() => {
-                  removeCustomBackgroundFile(tile['background-image'].replace(/\\(\s)/g, ' '));
+                  removeCustomBackgroundFile(tile['background-image-path'].replace(/\\(\s)/g, ' '));
                   upsertCustomBackgrounds(setCustomThemes);
                 }}
               />
