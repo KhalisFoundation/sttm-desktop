8aef6797c99bc123efd8566684aa58f4be1760e3
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2023-05-17 15:37:44 +0530

    fix: move the authentication dialog position above settings - use i18n locale for descriptions in the login dialog - show the offline message if not connected to internet

--- www/main/addons/auth-dialog/components/AuthDialog.jsx
+++ www/main/addons/auth-dialog/components/AuthDialog.jsx
@@ -4,13 +4,26 @@ import { ipcRenderer, shell } from 'electron';
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import keytar from 'keytar';
+import isOnline from 'is-online';
 
 import { Overlay } from '../../../common/sttm-ui';
 import { SP_API } from '../../../common/constants/api-urls';
 
+const remote = require('@electron/remote');
+
+const { i18n } = remote.require('./app');
+
 const AuthDialog = ({ onScreenClose, className }) => {
   const { userToken } = useStoreState((state) => state.app);
   const [userInfo, setUserInfo] = useState();
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    const onlineValue = isOnline();
+    onlineValue.then((data) => {
+      setConnected(data);
+    });
+  }, []);
 
   const fetchInfo = async () => {
     const response = await fetch(`${SP_API}/user`, {
@@ -30,39 +43,60 @@ const AuthDialog = ({ onScreenClose, className }) => {
   return (
     <Overlay onScreenClose={onScreenClose} className={className}>
       <div className="addon-wrapper sync-wrapper overlay-ui ui-sync-button auth-wrapper">
-        <div className="sync overlay-ui ui-sync-button">
-          <header className="sync-header">{userToken ? 'Logout' : 'Login'}</header>
-          <div className="sync-content-wrapper">
-            <div className="sync-content auth-content">
-              <h1>{userInfo ? `Welcome ${userInfo.firstname}` : 'Login'}</h1>
-              <p>{userInfo && `Logged in as ${userInfo.email}`}</p>
-              {userToken ? (
-                <button
-                  className="button auth-button logout-button"
-                  onClick={async () => {
-                    await keytar.deletePassword('sttm-desktop', 'userToken');
-                    ipcRenderer.emit('userToken', '');
-                    setUserInfo('');
-                    onScreenClose();
-                  }}
-                >
-                  <i className="fa-solid fa-right-from-bracket"></i>
-                  Logout
-                </button>
-              ) : (
-                <button
-                  className="button auth-button login-button"
-                  onClick={() => {
-                    shell.openExternal(`${SP_API}/login/sso`);
-                  }}
-                >
-                  <i className="fa-solid fa-right-to-bracket"></i>
-                  Login
-                </button>
-              )}
+        {connected ? (
+          <div className="sync overlay-ui ui-sync-button">
+            <header className="sync-header">
+              {userToken ? i18n.t('AUTH.LOGOUT_LABEL') : i18n.t('AUTH.LOGIN_LABEL')}
+            </header>
+            <div className="sync-content-wrapper">
+              <div className="sync-content auth-content">
+                <h1>
+                  {userInfo
+                    ? i18n.t('AUTH.LOGGED_IN_GREETING', { firstName: userInfo.firstname })
+                    : i18n.t('AUTH.LOGIN_LABEL')}
+                </h1>
+                <p>
+                  {userInfo
+                    ? i18n.t('AUTH.LOGGED_IN_DESC', { email: userInfo.email })
+                    : i18n.t('AUTH.LOGIN_DESC')}
+                </p>
+                {userToken ? (
+                  <button
+                    className="button auth-button logout-button"
+                    onClick={async () => {
+                      await keytar.deletePassword('sttm-desktop', 'userToken');
+                      ipcRenderer.emit('userToken', '');
+                      setUserInfo('');
+                      onScreenClose();
+                    }}
+                  >
+                    <i className="fa-solid fa-right-from-bracket"></i>
+                    {i18n.t('AUTH.LOGOUT_LABEL')}
+                  </button>
+                ) : (
+                  <button
+                    className="button auth-button login-button"
+                    onClick={() => {
+                      shell.openExternal(`${SP_API}/login/sso`);
+                    }}
+                  >
+                    <i className="fa-solid fa-right-to-bracket"></i>
+                    {i18n.t('AUTH.LOGIN_LABEL')}
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="sync overlay-ui ui-sync-button">
+            <header className="sync-header">{i18n.t('AUTH.LOGIN_LABEL')}</header>
+            <div className="sync-content-wrapper">
+              <div className="sync-content auth-content">
+                <p>{i18n.t('AUTH.INTERNET_ERR')}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </Overlay>
   );
