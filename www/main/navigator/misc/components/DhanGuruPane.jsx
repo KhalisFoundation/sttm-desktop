c84709420759cc204455da289233e43a855e0e80
Amanjot Singh <theamanjs@gmail.com>
2023-05-25 17:03:12 +0530

    feat: translation on dhan guru slide

--- www/main/navigator/misc/components/DhanGuruPane.jsx
+++ www/main/navigator/misc/components/DhanGuruPane.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { useStoreActions, useStoreState } from 'easy-peasy';
+import { ipcRenderer } from 'electron';
 import insertSlide from '../../../common/constants/slidedb';
 import { classNames } from '../../../common/utils';
 import tingle from '../../../../assets/js/vendor/tingle';
@@ -13,14 +14,18 @@ const analytics = remote.getGlobal('analytics');
 const { gurus } = insertSlide.dropdownStrings;
 
 export const DhanGuruPane = ({ className }) => {
-  const { isMiscSlide, miscSlideText, isAnnoucement, shortcuts } = useStoreState(
-    (state) => state.navigator,
-  );
-  const { setIsMiscSlide, setMiscSlideText, setIsAnnoucement, setShortcuts } = useStoreActions(
-    (state) => state.navigator,
-  );
+  const { isMiscSlide, miscSlideText, isAnnoucement, isMiscSlideGurmukhi, shortcuts } =
+    useStoreState((state) => state.navigator);
+  const {
+    setIsMiscSlide,
+    setMiscSlideText,
+    setIsAnnoucement,
+    setIsMiscSlideGurmukhi,
+    setShortcuts,
+  } = useStoreActions((state) => state.navigator);
   const { shortcutTray } = useStoreState((state) => state.userSettings);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentDhanGuruIndex, setCurrentDhanGuruIndex] = useState(null);
 
   const refsByGuruIndex = useMemo(() => {
     const refs = {};
@@ -40,8 +45,8 @@ export const DhanGuruPane = ({ className }) => {
   };
 
   const addDhanGuruSlide = (e) => {
-    if (isAnnoucement) {
-      setIsAnnoucement(false);
+    if (!isAnnoucement) {
+      setIsAnnoucement(true);
     }
     if (typeof e === 'object') {
       addMiscSlide(e.target.value);
@@ -81,7 +86,12 @@ export const DhanGuruPane = ({ className }) => {
         const buttonOnClick = () => {
           gurus.forEach((guru, index) => {
             document.querySelector(`#guru${index}`).onclick = () => {
-              addDhanGuruSlide(insertSlide.slideStrings.dhanguruStrings[index]);
+              addDhanGuruSlide(
+                insertSlide.slideStrings.dhanguruStrings[index][
+                  isMiscSlideGurmukhi ? 'gurmukhi' : 'english'
+                ],
+              );
+              setCurrentDhanGuruIndex(index);
               modal.close();
               setIsModalOpen(false);
               modal.destroy();
@@ -97,6 +107,34 @@ export const DhanGuruPane = ({ className }) => {
     }
   };
 
+  const getGuruIndex = (index) => {
+    if (index < 9) {
+      return `0${index + 1}`;
+    }
+    return `${index + 1}`;
+  };
+
+  const insertDhanGuru = (index) => {
+    const { english, gurmukhi } = insertSlide.slideStrings.dhanguruStrings[index];
+    setCurrentDhanGuruIndex(index);
+    if (isMiscSlideGurmukhi) {
+      addDhanGuruSlide(gurmukhi);
+    } else {
+      addDhanGuruSlide(english);
+    }
+  };
+
+  const toggleDhanGuruLanguage = (event) => {
+    if (isMiscSlideGurmukhi !== event.target.checked) {
+      setIsMiscSlideGurmukhi(event.target.checked);
+      addDhanGuruSlide(
+        insertSlide.slideStrings.dhanguruStrings[currentDhanGuruIndex][
+          event.target.checked ? 'gurmukhi' : 'english'
+        ],
+      );
+    }
+  };
+
   useEffect(() => {
     if (shortcuts.openDhanGuruSlide) {
       showDhanGuruModal();
@@ -107,35 +145,55 @@ export const DhanGuruPane = ({ className }) => {
     }
   }, [shortcuts]);
 
-  const getGuruIndex = (index) => {
-    if (index < 9) {
-      return `0${index + 1}`;
+  useEffect(() => {
+    if (isMiscSlide) {
+      ipcRenderer.send('show-misc-text', {
+        text: miscSlideText,
+        isGurmukhi: isMiscSlideGurmukhi,
+        isAnnoucement,
+      });
     }
-    return `${index + 1}`;
-  };
+  }, [miscSlideText, isMiscSlide, isMiscSlideGurmukhi, isAnnoucement]);
 
   return (
-    <div className={classNames('dhan-guru-pane', className, shortcutTray && 'misc-pane-shrinked')}>
-      {gurus.map((guru, index) => (
-        <div
-          className="dhan-guru-button"
-          key={guru}
-          onClick={() => addDhanGuruSlide(insertSlide.slideStrings.dhanguruStrings[index])}
-          onMouseEnter={() => {
-            refsByGuruIndex[guru].current.classList.add('dhan-guru-button-prefix-hover');
-            refsByGuruIndex[guru].current.innerHTML = '';
-          }}
-          onMouseLeave={() => {
-            refsByGuruIndex[guru].current.classList.remove('dhan-guru-button-prefix-hover');
-            refsByGuruIndex[guru].current.innerHTML = getGuruIndex(index);
-          }}
-        >
-          <span className="dhan-guru-button-prefix" ref={refsByGuruIndex[guru]}>
-            {getGuruIndex(index)}
-          </span>
-          <span className="dhan-guru-button-text">{i18n.t(`INSERT.DHAN_GURU.${guru}`)}</span>
+    <div className={className}>
+      <div className="dhan-guru-language-switch">
+        <span>{i18n.t('INSERT.DHAN_GURU_IN_GURMUKHI')}</span>
+        <div className="switch">
+          <input
+            id="dhan-guru-language"
+            name="dhan-guru-language"
+            type="checkbox"
+            checked={isMiscSlideGurmukhi}
+            onChange={toggleDhanGuruLanguage}
+          />
+          <label htmlFor="dhan-guru-language" />
         </div>
-      ))}
+      </div>
+      <div className={classNames('dhan-guru-pane', shortcutTray && 'misc-pane-shrinked')}>
+        {gurus.map((guru, index) => (
+          <div
+            className="dhan-guru-button"
+            key={guru}
+            onClick={() => {
+              insertDhanGuru(index);
+            }}
+            onMouseEnter={() => {
+              refsByGuruIndex[guru].current.classList.add('dhan-guru-button-prefix-hover');
+              refsByGuruIndex[guru].current.innerHTML = '';
+            }}
+            onMouseLeave={() => {
+              refsByGuruIndex[guru].current.classList.remove('dhan-guru-button-prefix-hover');
+              refsByGuruIndex[guru].current.innerHTML = getGuruIndex(index);
+            }}
+          >
+            <span className="dhan-guru-button-prefix" ref={refsByGuruIndex[guru]}>
+              {getGuruIndex(index)}
+            </span>
+            <span className="dhan-guru-button-text">{i18n.t(`INSERT.DHAN_GURU.${guru}`)}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
