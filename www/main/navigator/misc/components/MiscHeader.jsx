2b85984fe53213dd2a7cc7f23f1946b31af5dbeb
Amanjot Singh <theamanjs@gmail.com>
2023-05-25 11:50:49 +0530

    feat: add dhan guru tab on misc pane

--- www/main/navigator/misc/components/MiscHeader.jsx
+++ www/main/navigator/misc/components/MiscHeader.jsx
@@ -3,18 +3,22 @@ import { useStoreState, useStoreActions } from 'easy-peasy';
 
 export const MiscHeader = () => {
   const { currentMiscPanel } = useStoreState((state) => state.navigator);
-
   const { setCurrentMiscPanel } = useStoreActions((state) => state.navigator);
 
   const isHistory = currentMiscPanel === 'History';
   const isInsert = currentMiscPanel === 'Insert';
   const isOther = currentMiscPanel === 'Others';
+  const isDhanGuru = currentMiscPanel === 'DhanGuru';
 
   const setTab = (tabName) => {
     if (tabName !== currentMiscPanel) {
       setCurrentMiscPanel(tabName);
     }
   };
+
+  const getTurbanIcon = () =>
+    isDhanGuru ? 'assets/img/icons/turban-filled.png' : 'assets/img/icons/turban-outline.png';
+
   return (
     <div className="misc-header">
       <a
@@ -37,6 +41,15 @@ export const MiscHeader = () => {
             Insert
           </span>
         </i>
+      </a>
+      <a
+        className={`misc-button ${isDhanGuru ? 'misc-active' : ''}`}
+        onClick={() => setTab('DhanGuru')}
+      >
+        <img className="turban-icon" src={getTurbanIcon()} alt="Dhan Guru" />
+        <span className="Icon-label" key="DhanGuru">
+          Gurus
+        </span>
       </a>
       <a className={`misc-button ${isOther ? 'misc-active' : ''}`} onClick={() => setTab('Others')}>
         <i className="fa fa-ellipsis-h">
