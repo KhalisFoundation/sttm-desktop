062405bd7d184d02d54a2cf2a501ff187619e439
Amanjot Singh <theamanjs@gmail.com>
2023-05-31 16:28:55 +0530

    feat: add dhan guru to single display and minor ui fixes

--- www/main/navigator/single-display/singleDisplayContent.jsx
+++ www/main/navigator/single-display/singleDisplayContent.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { useStoreState } from 'easy-peasy';
-import { HistoryPane, OtherPane, InsertPane } from '../misc/components';
+import { HistoryPane, OtherPane, InsertPane, DhanGuruPane } from '../misc/components';
 import SearchPane from '../search/components/SearchPane';
 import ShabadPane from '../shabad/ShabadPane';
 
 export const singleDisplayContent = () => {
-  const { singleDisplayActiveTab } = useStoreState(state => state.navigator);
-  const renderSingleTab = tabName => {
+  const { singleDisplayActiveTab } = useStoreState((state) => state.navigator);
+  const renderSingleTab = (tabName) => {
     const components = (
       <>
         <SearchPane className={tabName === 'search' ? '' : 'd-none'} />
         <ShabadPane className={tabName === 'shabad' ? '' : 'd-none'} />
         <HistoryPane className={tabName === 'history' ? '' : 'd-none'} />
         <OtherPane className={tabName === 'other' ? '' : 'd-none'} />
+        <DhanGuruPane className={tabName === 'dhan-guru' ? '' : 'd-none'} />
         <InsertPane className={tabName === 'insert' ? '' : 'd-none'} />
       </>
     );
