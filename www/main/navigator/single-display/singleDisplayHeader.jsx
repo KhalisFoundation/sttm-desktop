fb29b90955974d79f59e7bdb2640e59c70064c3f
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2023-11-27 21:38:34 +0530

    fix: move the history sort filter in header to avoid overlap

--- www/main/navigator/single-display/singleDisplayHeader.jsx
+++ www/main/navigator/single-display/singleDisplayHeader.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useStoreState, useStoreActions } from 'easy-peasy';
 
 export const singleDisplayHeader = () => {
-  const { singleDisplayActiveTab, minimizedBySingleDisplay } = useStoreState(
+  const { singleDisplayActiveTab, minimizedBySingleDisplay, historyOrder } = useStoreState(
     (state) => state.navigator,
   );
-  const { setMinimizedBySingleDisplay } = useStoreActions((state) => state.navigator);
+  const { setMinimizedBySingleDisplay, setHistoryOrder } = useStoreActions(
+    (state) => state.navigator,
+  );
 
   const getActiveTab = (tabName) => {
     let component;
@@ -57,6 +59,22 @@ export const singleDisplayHeader = () => {
   return (
     <div className="header-controller">
       <span>{getActiveTab(singleDisplayActiveTab)}</span>
+      {singleDisplayActiveTab === 'history' && (
+        <div className="history-order">
+          <div className="history-order-select">
+            <label>Sort by: </label>
+            <select
+              value={historyOrder}
+              onChange={(e) => {
+                setHistoryOrder(e.target.value);
+              }}
+            >
+              <option value="newest">Newest First</option>
+              <option value="oldest">Oldest First</option>
+            </select>
+          </div>
+        </div>
+      )}
       <span onClick={toggleDisplayUI}>
         <i className={`fa fa-window-${minimizedBySingleDisplay ? 'maximize' : 'minimize'}`}></i>
       </span>
