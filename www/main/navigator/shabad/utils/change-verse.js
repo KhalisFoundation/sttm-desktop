e4ad8626f6be5fe73dd1da2b5f1eaaa9bc9eee5b
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2024-05-13 20:09:22 -0400

    feat: add the keyboard shortcuts in the multipane workspace; fixes issue #1931

--- www/main/navigator/shabad/utils/change-verse.js
+++ www/main/navigator/shabad/utils/change-verse.js
@@ -87,3 +87,69 @@ export const sendToBaniController = (
     }
   }
 };
+
+const skipIkOnkar = (shabadVerses, index) => {
+  if (shabadVerses[index]) {
+    // eslint-disable-next-line no-unsafe-optional-chaining
+    const { verse: gurmukhi } = shabadVerses[index]?.verse;
+    const { verseId } = shabadVerses[index];
+    if (verseId !== 1 && /^(<>)/gm.test(gurmukhi)) {
+      return index + 1;
+    }
+    return index;
+  }
+  return 0;
+};
+
+const skipMangla = (shabadVerses, index) => {
+  const gurmukhi = shabadVerses[index]?.verse;
+  if (/(mhlw [\w])|(mÚ [\w])/.test(gurmukhi) || (index === 0 && /sloku/.test(gurmukhi))) {
+    return skipIkOnkar(shabadVerses, index + 1);
+  }
+  return skipIkOnkar(shabadVerses, index);
+};
+
+export const intelligentNextVerse = (
+  filteredItems,
+  { activeVerseId, previousVerseIndex, setPreviousIndex, atHome, setHome, homeVerse },
+) => {
+  if (homeVerse) {
+    const currentVerseIndex = filteredItems.findIndex(({ verseId }) => verseId === activeVerseId);
+    let nextVerseIndex;
+
+    if (atHome) {
+      if (previousVerseIndex !== null) {
+        nextVerseIndex = previousVerseIndex + 1;
+        if (nextVerseIndex >= filteredItems.length) {
+          nextVerseIndex = 0;
+        }
+      } else {
+        nextVerseIndex = 0;
+      }
+      nextVerseIndex = skipMangla(filteredItems, nextVerseIndex);
+      if (nextVerseIndex === homeVerse) {
+        nextVerseIndex++;
+      }
+      setPreviousIndex(nextVerseIndex);
+      setHome(false);
+    } else {
+      nextVerseIndex = skipMangla(filteredItems, currentVerseIndex + 1);
+
+      if (nextVerseIndex >= filteredItems.length) {
+        nextVerseIndex = 0;
+      }
+      const currentVerseObj = filteredItems[currentVerseIndex];
+      const nextVerseObj = filteredItems[nextVerseIndex];
+
+      if (currentVerseObj.lineNo !== nextVerseObj.lineNo) {
+        nextVerseIndex = homeVerse;
+        setHome(true);
+      } else {
+        setPreviousIndex(nextVerseIndex);
+      }
+    }
+    const nextVerseId = filteredItems[nextVerseIndex].verseId;
+    return { verseId: nextVerseId, verseIndex: nextVerseIndex };
+  }
+  return null;
+};
